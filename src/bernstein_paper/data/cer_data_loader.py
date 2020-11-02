83401f9b40de5540222b9d856cc974ce8568a691
Marcel Arpogaus <marcel@arpogaus.de>
2020-11-02 16:24:04 +0100

    ADDED: docstring to load_data

--- src/bernstein_paper/data/cer_data_loader.py
+++ src/bernstein_paper/data/cer_data_loader.py
@@ -22,6 +22,50 @@ def load_data(data_path: str,
               cycle_length=10,
               shuffle_buffer_size=1000,
               seed=42):
+    """
+    Loads the preprocessed CER data and build the dataset.
+
+    :param      data_path:            The path to the folder containing the
+                                      train.csv and test.csv
+    :type       data_path:            str
+    :param      history_size:         The number of time steps of the historic
+                                      data a patch should contain
+    :type       history_size:         int
+    :param      horizon_size:         The number of time steps in the
+                                      prediction horizon a step should contain
+    :type       horizon_size:         int
+    :param      historic_columns:     The column names to used as historic
+                                      data.
+    :type       historic_columns:     Array
+    :param      horizon_columns:      The column names to be used as horizon
+                                      data.
+    :type       horizon_columns:      Array
+    :param      prediction_columns:   The columns to predict
+    :type       prediction_columns:   Array
+    :param      splits:               The data splits to be generated. At least
+                                      one of 'train', 'validate' or 'test'
+    :type       splits:               Array
+    :param      shift:                The amount of time steps by which the
+                                      window moves on each iteration
+    :type       shift:                int
+    :param      validation_split:     The amount of data reserved from the
+                                      training set for validation
+    :type       validation_split:     float
+    :param      batch_size:           The batch size
+    :type       batch_size:           int
+    :param      cycle_length:         The number of input elements that are
+                                      processed concurrently
+    :type       cycle_length:         int
+    :param      shuffle_buffer_size:  The shuffle buffer size
+    :type       shuffle_buffer_size:  int
+    :param      seed:                 The seed used by the pseudo random
+                                      generators
+    :type       seed:                 int
+
+    :returns:   A dict containing the windowed TensorFlow datasets generated
+                from csv file in `data_path` for the given `spits`.
+    :rtype:     dict
+    """
 
     # common ##################################################################
     data = {}
